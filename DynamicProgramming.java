376258cdd6d6f16b9a9bc9029cefec67f621f9c8
Yiyang Zhu <ererandkeke@gmail.com>
2018-01-31 20:31:29 -0500

    use table to find optimal solution

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -1,5 +1,4 @@
 class DynamicProgramming{
-    int[] opt;
     
     public static void main(String[] args){
         DynamicProgramming d = new DynamicProgramming();
@@ -8,20 +7,14 @@ class DynamicProgramming{
     
     void getOptimal(){
         int[] arr = {1,2,4,1,7,8,3};
-        opt = new int[arr.length];
+        int[] opt = new int[arr.length];
         opt[0] = arr[0];
-        opt[1] = Math.max(arr[0],arr[1]);
-        for(int i = 2; i < arr.length; i++){
-            opt[i] = recOptimal(arr, i);
+        opt[1] = Math.max(arr[0], arr[1]);
+        for(int i = 2; i < opt.length; i++){
+            int c = opt[i - 1];
+            int d = opt[i - 2] + arr[i];
+            opt[i] = Math.max(c,d);
         }
-        System.out.println("The optimal is: "+opt[arr.length-1]);
-    }
-        
-    int recOptimal(int[] arr, int j){
-        if(j == 0) return arr[0];
-        if(j == 1) return Math.max(arr[0],arr[1]);
-        int c = opt[j - 1];
-        int d = opt[j - 2] + arr[j];
-        return Math.max(c,d);
-    }
+        System.out.println("The optimal solution is: "+opt[opt.length-1]);
+    }           
 }
